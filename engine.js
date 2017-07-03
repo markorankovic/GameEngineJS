0d975fc311ed4e1819a0922d4d6a1c9341116d5d
Marko Rankovic <markocrush96@gmail.com>
2017-07-03 13:51:59 +0100

    Collision detection for circle redone

--- engine.js
+++ engine.js
@@ -98,153 +98,18 @@ class Circle extends Shape {
 		this.target;
 	}
 
-	getTranslatedPositionsAndVelocities(node, target) {
-		let r = this.radius;
-		let x = node.xPos+r;
-		let y = node.yPos+r;
-		let speed = Math.hypot(node.physicsBody.velocityY, node.physicsBody.velocityX);
-		let a = Math.atan2(node.physicsBody.velocityY, node.physicsBody.velocityX) - target.theta;
-		
-		let p = this.translateCentre(node, target);
-		let newX = p[0];
-		let newY = p[1];
-		let vX = speed*Math.cos(a);
-		let vY = speed*Math.sin(a);
-		return [newX, newY, vX, vY]; 
-	}
-
-	getPointOfCollisionForCircleAroundCorner(node, target, corner) {
-
-		if (node.physicsBody.velocityX == 0 && node.physicsBody.velocityY == 0) {
-			return [NaN, NaN];
-		}
-		let r = this.radius;
-		let translatedPositionsAndVelocities = this.getTranslatedPositionsAndVelocities(node, target);
-		
-		let velocityY = translatedPositionsAndVelocities[3];
-		let velocityX = translatedPositionsAndVelocities[2];
-
-		// target properties
-		let tX = corner[0];
-		let tY = corner[1];
-
-		// node properties
-		let cX = translatedPositionsAndVelocities[0]-tX;
-		let cY = translatedPositionsAndVelocities[1]-tY;
-		let vX = velocityX;
-		let vY = velocityY;
-		let dir = Math.atan2(vY, vX);
-
-		// Wolfram's algorithm
-
-		// Points in between for which a line may intersect a circle 
-		let x1 = cX;
-		let y1 = cY;
-
-		let x2 = cX + 1000*Math.cos(dir);
-		let y2 = cY + 1000*Math.sin(dir);
-
-		let dx = x2 - x1;
-		let dy = y2 - y1;
-		let dr = Math.hypot(dx, dy); 
-		let D = x1*y2 - x2*y1;
-		let discriminant = Math.round((r*r*dr*dr) - (D*D));
-
-		// Collision point
-		let x = (D*dy - dx * Math.sqrt(discriminant)) / (dr*dr) + tX;
-		let y = (-D*dx - dy*Math.sqrt(discriminant)) / (dr*dr) + tY;
-
-		let radius = Math.hypot(x-(target.xPos+target.shape.width/2), y-(target.yPos+target.shape.height/2));
-		let theta = Math.atan2((y-(target.yPos+target.shape.height/2)),(x-(target.xPos+target.shape.width/2))) + target.theta;
-	
-		// Translate back
-		x = target.xPos+target.shape.width/2 + radius * Math.cos(theta);
-		y = target.yPos+target.shape.height/2 + radius * Math.sin(theta);
-
-		// Check if direction of the circle is towards the point
-		let sine = node.yPos+r - y;
-		sine = (sine < 1 && sine > 0) || (sine > -1 && sine < 0) ? Math.abs(Math.round(sine)) : sine;
-		let cos = node.xPos+r - x;
-		cos = cos < 1 && cos > 0 ? Math.round(cos) : cos;
-
-		let angleOfPointRelativeToCircle = Math.atan2(sine, cos);
-		let a = Math.atan2(node.physicsBody.velocityY, node.physicsBody.velocityX);
-
-		if (((angleOfPointRelativeToCircle > 0 && a > 0) || (angleOfPointRelativeToCircle < 0 && a < 0) || (angleOfPointRelativeToCircle == a))) {
-			return [NaN, NaN];
-		}
-
-		return [x, y];
-
-	}		
-
-	collisionWithCorner(node, target, centreX, centreY) {
-		let nodeXCenter = centreX;
-		let nodeYCenter = centreY;
-
-		let p1 = [target.xPos, target.yPos + target.shape.height];  
-		let p2 = [target.xPos + target.shape.width, target.yPos + target.shape.height];  
-		let p3 = [target.xPos + target.shape.width, target.yPos + target.shape.height];  
-		let p4 = [target.xPos + target.shape.width, target.yPos];  
-		let p5 = [target.xPos + target.shape.width, target.yPos];  
-		let p6 = [target.xPos, target.yPos];  
-		let p7 = [target.xPos, target.yPos];  
-		let p8 = [target.xPos, target.yPos + target.shape.height];  
-
-		let c1 = [target.xPos, target.yPos + target.shape.height];
-		let c2 = [target.xPos + target.shape.width, target.yPos + target.shape.height];
-		let c3 = [target.xPos + target.shape.width, target.yPos];
-		let c4 = [target.xPos, target.yPos];
-
-		let withInP1AndP2 = ((nodeXCenter) >= p1[0] && (nodeXCenter) <= p2[0]) && ((nodeYCenter-this.radius) <= p1[1]);
-		let withInP3AndP4 = (nodeXCenter-this.radius) <= p3[0] && ((nodeYCenter) <= p3[1]) && ((nodeYCenter) >= p4[1]);
-		let withInP5AndP6 = (nodeYCenter+this.radius) >= p6[1] && ((nodeXCenter) >= p6[0]) && ((nodeXCenter) <= p5[0]);
-		let withInP7AndP8 = (nodeXCenter+this.radius) >= p7[0] && ((nodeYCenter) <= p8[1]) && ((nodeYCenter) >= p7[1]);
-
-		if ((withInP1AndP2 && withInP5AndP6) || (withInP3AndP4 && withInP7AndP8)) {
-			return true;
-		}
-
- 		let corners = [c1, c2, c3, c4];
-
-	for (let i = 0; i < corners.length; i++) {
-		let distance = Math.hypot(nodeXCenter - corners[i][0], nodeYCenter - corners[i][1]);
-		// Check c1
-		if (i == 0) {
-		if ((distance < this.radius) && nodeXCenter <= target.xPos && nodeXCenter >= target.xPos-this.radius && nodeYCenter >= target.yPos+target.shape.height) {
-			return true;
-		}
-		}
-		if (i == 1) {
-		// Check c2
-		if ((distance < this.radius) && nodeXCenter <= target.xPos+target.shape.width+this.radius && nodeXCenter >= target.xPos+target.shape.width && nodeYCenter >= target.yPos+target.shape.height) {
-			return true;
-		}
-		}
-		if (i == 2) {
-		// Check c3
-		if ((distance < this.radius) && nodeXCenter >= target.xPos+target.shape.width && nodeXCenter <= target.xPos+target.shape.width+this.radius && nodeYCenter <= target.yPos && nodeYCenter >= target.yPos-this.radius) {
-			return true;
-		}
-		}
-		if (i == 3) {
-		// Check c4
-		if ((distance < this.radius) && nodeXCenter <= target.xPos && nodeXCenter >= target.xPos-this.radius && nodeYCenter <= target.yPos && nodeYCenter >= target.yPos-this.radius) {
-			return true;
-		}
-		}
-	}
-		return false;
-
-	}
-
-
 	onRectHit(node, target) { 
-		let cPoint = target.shape.translateCentre(node, target);
-		if (this.collisionWithCorner(node, target, Math.round(cPoint[0]), Math.round(cPoint[1]))) {
-			return true;
-		}
-		return false;
+		let r = this.radius;
+		let Circle = this.translateCentre(node, target);
+		let CircleX = Circle[0];
+		let CircleY = Circle[1];
+		let RectX = target.xPos;
+		let RectY = target.yPos;
+		let RectWidth = target.shape.width;
+		let RectHeight = target.shape.height;
+		let DeltaX = CircleX - Math.max(RectX, Math.min(CircleX, RectX + RectWidth));
+		let DeltaY = CircleY - Math.max(RectY, Math.min(CircleY, RectY + RectHeight));
+		return (DeltaX * DeltaX + DeltaY * DeltaY) <= (r * r);
 	}
 
 	onCircHit(node, target) {
@@ -585,7 +450,7 @@ class Scene extends SpriteNode {
 
 		var rect3_1 = new SpriteNode(400, 340, 0, 'rect3_1');
 		rect3_1.setShape(new Rect(400, 100, 3, 'green', 'black'));
-		rect3_1.rotateBy(1);
+		//rect3_1.rotateBy(1);
 
 		this.addChild(rect3_1);
 		this.addChild(rect3);
@@ -617,7 +482,6 @@ class PhysicsBody {
 		this.angularVelocity = 0;
 		this.mass = mass;
 		this.alpha = 0; 
-		this.isTouching2 = false;
 	}
 
 	setGravity(gravity) {
@@ -643,293 +507,20 @@ class PhysicsBody {
 		this.node.changeXBy(this.node.shape.radius * (theta + alpha));
 	}
 
-	getClosestOfThePoints(collidingCorners, target) {
-		if (collidingCorners.length == 0) {
-			return NaN;
-		}
-		var closestDistances = [];
-		var original = [];
-		let tP = this.physicsMask.translateCentre(this.node, target);
-		for (var i = 0; i < collidingCorners.length; i++) {
-			let value = Math.hypot(tP[0]-collidingCorners[i][0], tP[1]-collidingCorners[i][1]);
-			closestDistances.push(value);
-			original.push(value);
-		}
-		let closestDistance = closestDistances.sort(function(a,b){return a - b})[0];
-		let index = original.indexOf(closestDistance); 
-		return collidingCorners[index];
-	}
-
-	getClosestCollidingCorner(target) {
-		let corners = [[target.xPos, target.yPos], [target.xPos, target.yPos+target.shape.height], [target.xPos+target.shape.width, target.yPos+target.shape.height], [target.xPos+target.shape.width, target.yPos]];
-		var collidingCorners = [];
-		for (var i = 0; i < corners.length; i++) {
-			if (this.physicsMask.getPointOfCollisionForCircleAroundCorner(this.node, target, corners[i])[0]) {
-				collidingCorners.push(corners[i]);
-			}  
-		}
-		return this.getClosestOfThePoints(collidingCorners, target);
-	}
-
-	isWithInBoundaries(p, corner1, corner2, isHorizontal) {
-		if (isHorizontal) {
-			return ((corner1[0] <= p[0]) && (corner2[0] >= p[0])) || ((corner1[0] >= p[0]) && (corner2[0] <= p[0]));
-		} 
-		return ((corner1[1] <= p[1]) && (corner2[1] >= p[1])) || ((corner1[1] >= p[1]) && (corner2[1] <= p[1]));
-	}
-
-	pointIsNotBehind(p, vX, vY, centreX, centreY) {
-		let dir1 = Math.round(Math.atan2(-(centreY) + p[1], -(centreX) + p[0]));
-		let dir2 = Math.round(Math.atan2(vY, vX));
-		return dir1 == dir2;
-	}
-
-	getPointOfIntersectionBetweenCorners(target, corner1, corner2) {
-		let isHorizontal = corner1[1] == corner2[1];
-		let r = this.physicsMask.radius;
-		let translation = this.physicsMask.getTranslatedPositionsAndVelocities(this.node, target);
-		let centreX = translation[0];
-		let centreY = translation[1];
-		let vX = translation[2];
-		let vY = translation[3];
-		let dir = Math.atan2(vY, vX);
-		let b = centreY - Math.tan(dir)*centreX;
-		if (isHorizontal) {
-			let x = (corner1[1] - b)/Math.tan(dir);
-			let dX = (r * 1/Math.tan(dir))*(corner1[1] > centreY ? -1 : 1);
-			let p = [x+dX, corner1[1]+r*(corner1[1] > centreY ? -1 : 1)]; 
-			return this.isWithInBoundaries(p, corner1, corner2, isHorizontal) && this.pointIsNotBehind(p, vX, vY, centreX, centreY) ? p : NaN;
-		} else { 
-			let y = Math.tan(dir)*corner1[0] + b;
-			let dY = (r * Math.tan(dir))*(corner1[0] > centreX ? -1 : 1);
-			let p = [corner1[0]+r*(corner1[0] > centreX ? -1 : 1), y+dY];
-			return this.isWithInBoundaries(p, corner1, corner2, isHorizontal) && this.pointIsNotBehind(p, vX, vY, centreX, centreY) ? p : NaN;
-		}
-		return NaN;
-	}
-
-	getAllIntersectingEdges(target) {
-		let corners = [[target.xPos, target.yPos], [target.xPos, target.yPos+target.shape.height], [target.xPos+target.shape.width, target.yPos+target.shape.height], [target.xPos+target.shape.width, target.yPos]];
-		var intersectingEdges = [];
-		for (let i = 0; i < corners.length; i++) {
-			let p = this.getPointOfIntersectionBetweenCorners(target, corners[i], corners[(i+1)%corners.length]);
-			if (p) {
-				intersectingEdges.push(p);
-			}
-		}	
-		return intersectingEdges;
-	}
-
-	getPointOnEdge(target) {
-		let intersectingEdges = this.getAllIntersectingEdges(target);
-		if (intersectingEdges.length > 0) {
-			let result = this.getClosestOfThePoints(intersectingEdges, target);
-			return result;
-		}
-		return NaN;
-	}
-
-	translateBack(point, target) {
-		if (!point) {
-			return NaN;
-		}
-		let r = this.physicsMask.radius;
-		let x = point[0]; 
-		let y = point[1];
-		let radius = Math.hypot(x-(target.xPos+target.shape.width/2), y-(target.yPos+target.shape.height/2));
-		let theta = Math.atan2((y-(target.yPos+target.shape.height/2)),(x-(target.xPos+target.shape.width/2))) + target.theta;
-		x = (target.xPos+(target.shape.width/2) + radius * Math.cos(theta));
-		y = (target.yPos+(target.shape.height/2) + radius * Math.sin(theta));
-		return [x, y];
-	}
-
-	centreDistanceToPoint(point) {
-		return Math.hypot(this.node.xPos+this.physicsMask.radius - point[0], this.node.yPos+this.physicsMask.radius - point[1]);
-	}
-
-	cornerIsCloser(points, hittingEdge) {
-		if (hittingEdge && points.length > 1) {
-			return this.centreDistanceToPoint(points[0]) > this.centreDistanceToPoint(points[1]);
-		} 
-		if (!hittingEdge && points.length == 1) {
-			return true;
-		}
-		return false;
-	}
-
-	isAtOneOfTheEdges(corners, r, target) {
-		let centre = this.physicsMask.translateCentre(this.node, target);
-		let centreX = centre[0];
-		let centreY = centre[1];
-		let edge1 = [corners[0], corners[1]];
-		let edge2 = [corners[1], corners[2]];
-		let edge3 = [corners[2], corners[3]];
-		let edge4 = [corners[3], corners[0]];
-		let edges = [edge1, edge2, edge3, edge4];
-
-		for (var i = 0; i < edges.length; i++) {
-			if (edges[i][0][1] == edges[i][1][1]) {
-				// Horizontal
-				if (Math.round(centreY) == (edges[i][0][1] == target.yPos ? edges[i][0][1]-r : edges[i][0][1]+r)) {
-					if (centreX >= edge1[0][0] && centreX <= edge1[1][0]) {
-						return true;
-					}
-					if (centreX <= edge3[0][0] && centreX >= edge3[1][0]) {
-						return true;
-					}
-				}
-			} else {
-				// Vertical
-				if (Math.round(centreX) == target.xPos-r || Math.round(centreX) == target.xPos+target.shape.width+r) {
-					if (centreY <= edge2[0][1] && centreY >= edge2[1][1]) {
-						return true;
-					}
-					if (centreY >= edge4[0][1] && centreY <= edge4[1][1]) {
-						return true;
-					}
-				}
-			}
-		}
-
-		return false;
-	}
-
-	isAtOneOfTheCorners(corners, r, target) {
-		let centre = this.physicsMask.translateCentre(this.node, target);
-		let centreX = centre[0];
-		let centreY = centre[1];
-		for (var i = 0; i < corners.length; i++) {
-			let corner = corners[i];
-			if (Math.round(Math.hypot(centreX-corner[0], centreY-corner[1])) == r) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	centreAlreadyAtCollisionPoint(r, target) {
-		let w = target.shape.width;
-		let h = target.shape.height;		
-		let corners = [[target.xPos, target.yPos+h], [target.xPos+w, target.yPos+h], [target.xPos+w, target.yPos], [target.xPos, target.yPos]];
-		let isAtACorner = this.isAtOneOfTheCorners(corners, r, target);
-		let isAtAnEdge = this.isAtOneOfTheEdges(corners, r, target);
-		return isAtAnEdge || isAtACorner;
-	}
-
-	getPointOfCollision(r, target) {
-		if (this.velocityX == 0 && this.velocityY == 0) {
-			return NaN;
-		}
-		let pointOnEdge = this.translateBack(this.getPointOnEdge(target), target);
-		var points = [];
-		let hittingEdge = false;
-		if (pointOnEdge) {
-			points.push(pointOnEdge);
-			hittingEdge = true;
-		}
-		let corner = this.getClosestCollidingCorner(target);
-		if (corner) {
-			points.push(this.physicsMask.getPointOfCollisionForCircleAroundCorner(this.node, target, corner));
-		}
-		if (this.cornerIsCloser(points, hittingEdge) && points.length > 0) {
-			return points.length > 1 ? points[1] : points[0];
-		} else if (hittingEdge) {
-			return points[0];
-		}
-		return NaN;
-	}
 
 
-	applyGravitation() {
-		this.changeYVelocityBy(this.gravity);
-	}
+	
 
 
-	notMoving() {
-		return this.velocityX == 0 && this.velocityY == 0;
-	}
 
-
-	getPointsOfCollisionBetweenVelocity(r, target) {
-		this.node.changeXBy(-this.velocityX);
-		this.node.changeYBy(-this.velocityY);
-
-		let currentPointOfCollision = this.getPointOfCollision(r, target);
-
-		this.node.changeXBy(this.velocityX);
-		this.node.changeYBy(this.velocityY);
-
-		let nextPointOfCollision = this.getPointOfCollision(r, target); 
-		return [currentPointOfCollision, nextPointOfCollision];
-	}
-
-
-	currentAndNextPointOfCollisionAreTheSame(r, target) {
-
-		if (this.notMoving()) {
-			return true;
-		}
-
-		let pointsOfCollisionBetweenVelocity = this.getPointsOfCollisionBetweenVelocity(r, target);
-		let currentPointOfCollision = pointsOfCollisionBetweenVelocity[0];
-		let nextPointOfCollision = pointsOfCollisionBetweenVelocity[1];
-
-		if (!currentPointOfCollision) {
-			return true;
-		}
-
-		let xPointsTheSame = Math.round(currentPointOfCollision[0]) == Math.round(nextPointOfCollision[0]);
-		let yPointsTheSame = Math.round(currentPointOfCollision[1]) == Math.round(nextPointOfCollision[1]);
-
-		return xPointsTheSame && yPointsTheSame;
-	}
-
-
-	circleIsHit(r, target) {
-
-		if (!this.currentAndNextPointOfCollisionAreTheSame(r, target)) {
-			return true;
-		}
-		return false;
-
-	}
-
-
-	bounce() {
-		this.velocityX = -this.velocityX;
-		this.velocityY = -this.velocityY;
-	}
-
-	handleCollision(r, target) {
-		let circleIsHit = this.circleIsHit(r, target);
-		if (circleIsHit) {
-			console.log("circleIsHit");
-			this.node.changeXBy(-this.velocityX);
-			this.node.changeYBy(-this.velocityY);
-			this.bounce();
-		}
-	}
 
 
 
 
 	simulateCircle(r, target) {
-		this.applyGravitation();
+		console.log(this.physicsMask.isHit(this.node, this.node.parent));
 		this.move();
-		this.handleCollision(r, target);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
